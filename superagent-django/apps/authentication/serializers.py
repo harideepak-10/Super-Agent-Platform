826f4664858f2e4a67862a71cb100509cd886cb8
Deepak <harideepak@krypsos.tech>
2026-07-02 20:19:26 +0530

    fix: avatar upload saves to disk, stores URL in avatar_url

--- superagent-django/apps/authentication/serializers.py
+++ superagent-django/apps/authentication/serializers.py
@@ -6,19 +6,10 @@ User = get_user_model()
 
 
 class UserSerializer(serializers.ModelSerializer):
-    avatar_url = serializers.SerializerMethodField()
-
     class Meta:
         model = User
         fields = ["id", "email", "name", "avatar_url", "created_at"]
         read_only_fields = ["id", "created_at"]
-
-    def get_avatar_url(self, obj):
-        request = self.context.get("request")
-        if obj.avatar:
-            url = obj.avatar.url
-            return request.build_absolute_uri(url) if request else url
-        return obj.avatar_url or None
 
 
 class RegisterSerializer(serializers.ModelSerializer):
